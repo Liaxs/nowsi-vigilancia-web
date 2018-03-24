b7d9a520123103fd036f5d724b8069f077713fcc
Elias Saavedra <elias.saavedra.lol@gmail.com>
2018-03-24 11:55:04 -0300

    se cambia base de datos

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBuUG3ksrLRuXyjcLhOpM0deV0Cow9YXng",
-    authDomain: "mapas-7c0d0.firebaseapp.com",
-    databaseURL: "https://mapas-7c0d0.firebaseio.com",
-    projectId: "mapas-7c0d0",
-    storageBucket: "mapas-7c0d0.appspot.com",
-    messagingSenderId: "439976486711"
+   	apiKey: "AIzaSyDpsIWZtja9DNzS2yMLlIbCdIqtu9fWwD8",
+    authDomain: "vigilancia-e2011.firebaseapp.com",
+    databaseURL: "https://vigilancia-e2011.firebaseio.com",
+    projectId: "vigilancia-e2011",
+    storageBucket: "",
+    messagingSenderId: "1001964929909"
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
